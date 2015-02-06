68baf3216a1a2124b5beba989d4dac65e056a474
maciano <mariano.giardina@gmail.com>
2015-02-05 21:08:18 -0300

    Update ProductList.java
    Clean up comments

--- src/test/java/com/Tesis/pages/ProductList.java
+++ src/test/java/com/Tesis/pages/ProductList.java
@@ -15,38 +15,14 @@ public class ProductList {
 	// Definimos los atributos de la clase = Elementos de la pagina
 	@FindBy(how = How.CSS, using = "div.box-collateral.box-description > h2")
 	private WebElement Detalles;
-/*	
-	@FindBy(how = How.XPATH, using = "//li/a/img")
-	private WebElement item11;
-	@FindBy(how = How.XPATH, using = "//li[2]/a/img")
-	private WebElement item12;
-	@FindBy(how = How.XPATH, using = "//li[3]/a/img")
-	private WebElement item13;
-	
-	@FindBy(how = How.XPATH, using = "//ul[2]/li/a/img")
-	private WebElement item21;
-	@FindBy(how = How.XPATH, using = "//ul[2]/li[2]/a/img")
-	private WebElement item22;
-	@FindBy(how = How.XPATH, using = "//ul[2]/li[3]/a/img")
-	private WebElement item23;
-	
-	@FindBy(how = How.XPATH, using = "//ul[3]/li/a/img")
-	private WebElement item31;
-	@FindBy(how = How.XPATH, using = "//ul[3]/li[2]/a/img")
-	private WebElement item32;
 
-	
-	@FindBy(how = How.XPATH, using = "//ul[3]/li[3]/a/img")
-	private WebElement item33;
-*/	
 	//******************************************************************
-	
 	//Constructor
 	public ProductList(WebDriver driver) {
 		this.driver = driver;
 	}
 	
-	//Definimos los mÈtodos = Acciones de la p·gina
+	//Definimos los m√©todos = Acciones de la p√°gina
 	public void clickOn(WebElement item) {
 		item.click();
 	}
@@ -63,23 +39,17 @@ public class ProductList {
 		}
 		
 		path+="/a/img";
-//		System.out.println("path:"+path);
 		WebElement item = driver.findElement(By.xpath(path));
 		
 		return item;
-		
 	}
 	
 	// Este metodo retorna la page class a la que vamos luego que esta se ejecute bien
 	public ProductDescription comprarProducto(int row, int high) {
-		
 		WebElement item = convertRowColToElement(row,high);
 		clickOn(item);
 		
-
 		Assert.assertEquals("Details", Detalles.getText());
 		return PageFactory.initElements(driver, ProductDescription.class);
 	}
-	
-	
 }
